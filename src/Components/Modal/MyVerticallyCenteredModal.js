996cbad9620e24b221525780e78f55d3fe3564b0
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-27 20:12:49 +0530

    create modal but not customized it

--- src/Components/Modal/MyVerticallyCenteredModal.js
+++ src/Components/Modal/MyVerticallyCenteredModal.js
@@ -1,20 +1,59 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
+import { ImCross } from "react-icons/im";
+import Title from "../Title/Title";
+import "./Modal.css";
 
 function MyVerticallyCenteredModal(props) {
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered="true"
-    >
-      <Modal.Header closeButton />
-     
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Modal
+        show={props.show}
+        onHide={props.onHide}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+        className="main-modal"
+      >
+        <Modal.Body>
+          <div className="main-modal-container">
+            <div className="modal-head">
+              <ImCross onClick={props.onHide} className="cancel-icon" />
+            </div>
+            <div className="modal-title">
+              <Title
+                title="Enter Your Delivery Address"
+                sup="Address Data & Types of Delivery"
+              />
+            </div>
+            <div className="form-container">
+              <form className="container">
+                <div className="row">
+                  <div className="col">1 of 3</div>
+                  <div className="col">3 of 3</div>
+                </div>
+                <div className="row">
+                  <div className="col">1 of 3</div>
+                  <div className="col">3 of 3</div>
+                </div>
+                <div className="row">
+                  <div className="col">1 of 3</div>
+                  <div className="col">3 of 3</div>
+                </div>
+                <div className="row">
+                  <div className="col">1 of 3</div>
+                  <div className="col">3 of 3</div>
+                </div>
+                <div className="form-button-container">
+                  <button type="button" className="btn btn-secondary">
+                    Save
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
 
