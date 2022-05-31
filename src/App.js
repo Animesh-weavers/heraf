d951b7784045c988ccfcb8d90291189d5e821f1a
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-31 12:08:24 +0530

    create title static container and code structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import DeliveryPage from "./Components/DeliveryAddress_Page/DeliveryPage";
+import PaymentCard_Page from "./Components/PaymentCard_Page/PaymentCard_Page";
 import PaymentPage from "./Components/Payment_Page/PaymentPage";
-
 
 const App = () => {
   return (
     <div className="App">
       <PaymentPage />
       <DeliveryPage />
+      <PaymentCard_Page />
     </div>
   );
 };
--- src/Components/PaymentCard_Page/PaymentCard_Page.js
+++ src/Components/PaymentCard_Page/PaymentCard_Page.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import './PaymentCard_Page.css'
+import React from "react";
+import "./PaymentCard_Page.css";
+import Title from "../Title/Title";
+import DeliveryStatus from "../DeliveryStatus/DeliveryStatus";
 
 const PaymentCard_Page = () => {
   return (
-    <div>PaymentCard_Page</div>
-  )
-}
+    <>
+      <div className="staticContainer">
+        <div className="static-header-container">
+          <div className="static-Title">
+            <Title
+              title="Choose a Payment method"
+              sup="You will not be charged,untill you review this order on the next page"
+            />
+          </div>
+          <div className="static-status-delivery">
+            <DeliveryStatus />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default PaymentCard_Page
+export default PaymentCard_Page;
