ef6cb15a79391ce27ddb976adfa2a9f5ea2d09e9
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-31 11:25:07 +0530

    create paymentcard_page js and css page

---
+++ src/Components/PaymentCard_Page/PaymentCard_Page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PaymentCard_Page.css'
+
+const PaymentCard_Page = () => {
+  return (
+    <div>PaymentCard_Page</div>
+  )
+}
+
+export default PaymentCard_Page
